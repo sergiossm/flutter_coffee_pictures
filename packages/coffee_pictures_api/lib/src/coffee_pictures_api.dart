7c9bc0cdd65f94d0ce65763aa16ebc99593800d0
Sergio Sanchez <sergio.ssm.97@gmail.com>
2022-12-06 00:09:19 +0100

    chore: http coffee pictures api

--- packages/coffee_pictures_api/lib/src/coffee_pictures_api.dart
+++ packages/coffee_pictures_api/lib/src/coffee_pictures_api.dart
@@ -1,5 +1,11 @@
 import 'package:coffee_pictures_api/coffee_pictures_api.dart';
 
+/// Exception thrown when fetchCoffeePicture fails.
+class CoffeePicturesRequestFailure implements Exception {}
+
+/// Exception thrown when file for a coffee picture is not found.
+class CoffeePictureFileNotFoundFailure implements Exception {}
+
 /// {@template coffee_pictures_api}
 /// The interface and models for an API providing access to coffee pictures.
 /// {@endtemplate}
--- packages/http_coffe_pictures_api/lib/src/http_coffe_pictures_api.dart
+++ packages/http_coffe_pictures_api/lib/src/http_coffe_pictures_api.dart
@@ -1,7 +1,38 @@
+import 'dart:convert';
+
+import 'package:coffee_pictures_api/coffee_pictures_api.dart';
+import 'package:http/http.dart' as http;
+
 /// {@template http_coffe_pictures_api}
 /// A Flutter implementation of the CoffeePicturesApi that uses HTTP.
 /// {@endtemplate}
-class HttpCoffePicturesApi {
+class HttpCoffePicturesApi extends CoffeePicturesApi {
   /// {@macro http_coffe_pictures_api}
-  const HttpCoffePicturesApi();
+  HttpCoffePicturesApi({http.Client? httpClient}) : _httpClient = httpClient ?? http.Client();
+
+  static const _baseUrlCoffeePictures = 'coffee.alexflipnote.dev/';
+
+  final http.Client _httpClient;
+
+  @override
+  Future<CoffeePicture> fetchCoffeePicture() async {
+    final coffeePicturesRequest = Uri.https(
+      _baseUrlCoffeePictures,
+      'random.json',
+    );
+
+    final coffeePicturesResponse = await _httpClient.get(coffeePicturesRequest);
+
+    if (coffeePicturesResponse.statusCode != 200) {
+      throw CoffeePicturesRequestFailure();
+    }
+
+    final bodyJson = jsonDecode(coffeePicturesResponse.body) as Map<String, dynamic>;
+
+    if (!bodyJson.containsKey('file')) {
+      throw CoffeePictureFileNotFoundFailure();
+    }
+
+    return CoffeePicture.fromJson(bodyJson);
+  }
 }
