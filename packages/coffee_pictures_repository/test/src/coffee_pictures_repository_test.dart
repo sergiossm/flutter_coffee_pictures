0dbc770213d462fd645e89cc267aa70ff020008c
Sergio Sanchez <sergio.ssm.97@gmail.com>
2022-12-06 01:17:02 +0100

    test: coffee pictures repo

--- packages/coffee_pictures_repository/test/src/coffee_pictures_repository_test.dart
+++ packages/coffee_pictures_repository/test/src/coffee_pictures_repository_test.dart
@@ -1,1 +1,70 @@
+import 'package:coffee_pictures_api/coffee_pictures_api.dart';
+import 'package:coffee_pictures_repository/coffee_pictures_repository.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
 
+class MockCoffeePicturesApi extends Mock implements CoffeePicturesApi {}
+
+class MockCoffeePicture extends Mock implements CoffeePicture {}
+
+void main() {
+  group(
+    'MockCoffeePicturesRepository',
+    () {
+      late CoffeePicturesApi coffeePicturesApi;
+      late CoffeePicturesRepository coffeePicturesRepository;
+
+      setUp(() {
+        coffeePicturesApi = MockCoffeePicturesApi();
+        coffeePicturesRepository = CoffeePicturesRepository(
+          coffeePicturesApi: coffeePicturesApi,
+        );
+      });
+
+      CoffeePicturesRepository createSubject() => CoffeePicturesRepository(
+            coffeePicturesApi: coffeePicturesApi,
+          );
+
+      group('constructor', () {
+        test('works properly', () {
+          expect(
+            createSubject,
+            returnsNormally,
+          );
+        });
+      });
+
+      group(
+        'fetchCoffeePicture',
+        () {
+          const file = 'https://coffee.alexflipnote.dev/BYf2UB0AGTM_coffee.jpg';
+
+          test('makes correct api request', () async {
+            final coffeePicture = MockCoffeePicture();
+            when(() => coffeePicture.file).thenReturn(file);
+            when(() => coffeePicturesApi.fetchCoffeePicture()).thenAnswer(
+              (_) async => coffeePicture,
+            );
+            try {
+              await coffeePicturesRepository.fetchCoffeePicture();
+            } catch (_) {}
+            verify(
+              () => coffeePicturesApi.fetchCoffeePicture(),
+            ).called(1);
+          });
+
+          test('throws when fetchCoffeePicture fails', () async {
+            final exception = Exception('oops');
+            when(() => coffeePicturesApi.fetchCoffeePicture()).thenThrow(
+              exception,
+            );
+            expect(
+              () async => coffeePicturesRepository.fetchCoffeePicture(),
+              throwsA(exception),
+            );
+          });
+        },
+      );
+    },
+  );
+}
