719d5b75e10a25cd243bad9282ed12cfa60ae743
Sergio Sanchez <sergio.ssm.97@gmail.com>
2022-12-08 20:05:46 +0100

    chore: coverage 82.1%

--- test/coffee_picture/view/coffee_picture_page_test.dart
+++ test/coffee_picture/view/coffee_picture_page_test.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:bloc_test/bloc_test.dart';
 import 'package:coffee_pictures_repository/coffee_pictures_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_coffee_pictures/coffee_picture/bloc/coffee_picture_bloc.dart';
 import 'package:flutter_coffee_pictures/coffee_picture/view/view.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,29 +11,19 @@ import 'package:mocktail/mocktail.dart';
 
 import '../../helpers/helpers.dart';
 
-// ignore: lines_longer_than_80_chars
-class MockCoffeePicturesRepository extends Mock
-    implements CoffeePicturesRepository {}
+class MockCoffeePicturesRepository extends Mock implements CoffeePicturesRepository {}
 
 class MockCoffeePicture extends Mock implements CoffeePicture {}
 
-// ignore: lines_longer_than_80_chars
-class MockCoffeePictureBloc
-    extends MockBloc<CoffeePictureEvent, CoffeePictureState>
-    implements CoffeePictureBloc {}
+class MockCoffeePictureBloc extends MockBloc<CoffeePictureEvent, CoffeePictureState> implements CoffeePictureBloc {}
 
 void main() {
-  late CoffeePicture coffeePicture;
   late CoffeePicturesRepository coffeePicturesRepository;
-  const file = 'https://coffee.alexflipnote.dev/BYf2UB0AGTM_coffee.jpg';
+  final coffeePicture = CoffeePicture(file: 'https://coffee.alexflipnote.dev/BYf2UB0AGTM_coffee.jpg');
 
   setUp(() {
     coffeePicturesRepository = MockCoffeePicturesRepository();
-    coffeePicture = MockCoffeePicture();
-    when(() => coffeePicture.file).thenReturn(file);
-    when(() => coffeePicturesRepository.fetchCoffeePicture()).thenAnswer(
-      (_) async => coffeePicture,
-    );
+    when(() => coffeePicturesRepository.fetchCoffeePicture()).thenAnswer((_) async => coffeePicture);
   });
 
   group('CoffeePicturePage', () {
@@ -41,19 +35,19 @@ void main() {
 
       expect(find.byType(CoffeePictureView), findsOneWidget);
     });
-
-    // testWidgets('renders CoffeePictureView', (widgetTester) async {
-    //   await widgetTester.pumpWidget(
-    //     RepositoryProvider.value(
-    //       value: coffeePicturesRepository,
-    //       child: const CoffeePicturePage(),
-    //     ),
-    //   );
-    //   expect(find.byType(CoffeePictureView), findsOneWidget);
-    // });
   });
 
   group('CoffeePictureView', () {
+    const refreshFloatingActionButtonKey = Key(
+      'coffeePictureView_refresh_floatingActionButton',
+    );
+    const downloadOutlinedButtonKey = Key(
+      'coffeePictureView_download_outlinedButton',
+    );
+    const emptySizedBoxKey = Key(
+      'coffeePictureView_empty_sizedBox',
+    );
+
     late CoffeePictureBloc coffeePictureBloc;
 
     setUp(() {
@@ -66,54 +60,91 @@ void main() {
       );
     });
 
-    // testWidgets(
-    //   'renders error Snackbar when status changes to failure',
-    //   (widgetTester) async {
-    //     whenListen<CoffeePictureState>(
-    //       coffeePictureBloc,
-    //       Stream.fromIterable([
-    //         const CoffeePictureState(),
-    //         const CoffeePictureState(
-    //           status: CoffeePictureStatus.failure,
-    //         ),
-    //       ]),
-    //     );
+    Widget buildSubject() {
+      return BlocProvider.value(
+        value: coffeePictureBloc,
+        child: const CoffeePictureView(),
+      );
+    }
 
-    //     await widgetTester.pumpApp(
-    //       const CoffeePicturePage(),
-    //       coffeePicturesRepository: coffeePicturesRepository,
-    //     );
-    //     await widgetTester.pumpAndSettle();
+    group('refresh floating action button ', () {
+      testWidgets('is rendered', (widgetTester) async {
+        await widgetTester.pumpApp(
+          buildSubject(),
+          coffeePicturesRepository: coffeePicturesRepository,
+        );
 
-    //     // expect(find.byType(SnackBar), findsOneWidget);
-    //     // expect(
-    //     //   find.descendant(
-    //     //     of: find.byType(SnackBar),
-    //     //     matching: find.text(l10n.coffeePictureErrorSnackbarText),
-    //     //   ),
-    //     //   findsOneWidget,
-    //     // );
-    //   },
-    // );
+        expect(find.byKey(refreshFloatingActionButtonKey), findsOneWidget);
 
-    // group('when coffee picture is not null', () {
-    //   setUp(() {
-    //     when(() => coffeePictureBloc.state).thenReturn(
-    //       CoffeePictureState(
-    //         status: CoffeePictureStatus.success,
-    //         coffeePicture: coffeePicture,
-    //       ),
-    //     );
-    //   });
+        final refreshFloatingActionButton = widgetTester.widget(find.byKey(refreshFloatingActionButtonKey));
+        expect(refreshFloatingActionButton, isA<FloatingActionButton>());
+      });
 
-    //   testWidgets('renders URL', (widgetTester) async {
-    //     await widgetTester.pumpApp(
-    //       const CoffeePicturePage(),
-    //       coffeePicturesRepository: coffeePicturesRepository,
-    //     );
+      testWidgets(
+          'calls CoffeePictureRefreshRequested '
+          'when is clicked', (widgetTester) async {
+        await widgetTester.pumpApp(
+          buildSubject(),
+          coffeePicturesRepository: coffeePicturesRepository,
+        );
 
-    //     expect(find.byType(Center), findsOneWidget);
-    //   });
-    // });
+        await widgetTester.tap(find.byKey(refreshFloatingActionButtonKey));
+
+        verify(() => coffeePictureBloc.add(const CoffeePictureRefreshRequested())).called(1);
+      });
+    });
+
+    group('download coffee picture button', () {
+      testWidgets(
+          'is rendered '
+          'when downloadStatus is initial', (widgetTester) async {
+        await widgetTester.pumpApp(
+          buildSubject(),
+          coffeePicturesRepository: coffeePicturesRepository,
+        );
+
+        expect(find.byKey(downloadOutlinedButtonKey), findsOneWidget);
+
+        final downloadOutlinedButton = widgetTester.widget(find.byKey(downloadOutlinedButtonKey));
+        expect(downloadOutlinedButton, isA<OutlinedButton>());
+      });
+
+      testWidgets(
+        'is not rendered '
+        'when downloadedStatus is success',
+        (widgetTester) async {
+          when(() => coffeePictureBloc.state).thenReturn(
+            CoffeePictureState(
+              status: CoffeePictureStatus.success,
+              coffeePicture: coffeePicture,
+              downloadStatus: CoffeePictureDownloadStatus.success,
+            ),
+          );
+
+          await widgetTester.pumpApp(
+            buildSubject(),
+            coffeePicturesRepository: coffeePicturesRepository,
+          );
+
+          expect(find.byKey(emptySizedBoxKey), findsOneWidget);
+
+          final emptySizedBox = widgetTester.widget(find.byKey(emptySizedBoxKey));
+          expect(emptySizedBox, isA<SizedBox>());
+        },
+      );
+
+      testWidgets(
+          'calls CoffeePictureDownloadRequested '
+          'when is clicked', (widgetTester) async {
+        await widgetTester.pumpApp(
+          buildSubject(),
+          coffeePicturesRepository: coffeePicturesRepository,
+        );
+
+        await widgetTester.tap(find.byKey(downloadOutlinedButtonKey));
+
+        verify(() => coffeePictureBloc.add(CoffeePictureDownloadRequested(coffeePicture))).called(1);
+      });
+    });
   });
 }
